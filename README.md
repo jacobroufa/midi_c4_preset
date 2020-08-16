e844a03cfb07ae2b7588431bcb18b936a34db8da
Jacob M. Roufa <jacob.roufa@gmail.com>
2020-08-15 21:24:07 -0500

    working C4 MIDI interaction with Trinket M0; update documentation

--- README.md
+++ README.md
@@ -8,27 +8,29 @@ Intended for use with the [Source Audio C4 Synth](https://www.sourceaudio.net/c4
 
 ### Hardware
 
-- Arduino Pro Micro (or other 32u4 chip)
+- Adafruit Trinket M0
 - TM1637 7-segment display
-- 4 position DIP switch
 - 2x momentary buttons
 - 1590A case (or similar)
 
-### Software
+### Software Dependencies
 
 - https://github.com/avishorp/TM1637
-- https://github.com/BlokasLabs/USBMIDI
+- https://github.com/gdsports/USB_Host_Library_SAMD
 
 ---
 
 ## Pin Assignments
 
-- 2 - Up Button
-- 3 - Down Button
-- 4 - LCD I/O
-- 5 - LCD Clock
-- 6-9 - DIP switch pins
+- 0 - Up Button
+- 1 - Down Button
+- 2 - LCD I/O
+- 3 - LCD Clock
 
 ---
 
 Prompted by [a discussion at The Gear Page](https://www.thegearpage.net/board/index.php?threads/source-audio-c4-homebrew-arduino-midi-controller.2078129). Thanks to user morrissey007 for the motivation. :)
+
+## Functionality
+
+Button 0 press will increment preset (default/startup = 0), Button 1 will decrement. Pressing both buttons simultaneously will display the channel (set to 0 in code). Uncomment L114 in order to allow this to be changed. Don't know if this is useful -- I may have the combined press do something different in the future.
--- midi_c4_preset.ino
+++ midi_c4_preset.ino
@@ -1,77 +1,102 @@
-#include <TM1637Display.h>
-#include <usbmidi.h>
+#include <TM1637Display.h> // https://github.com/avishorp/TM1637
+#include <usbh_midi.h> // https://github.com/gdsports/USB_Host_Library_SAMD
 
-const int BUTTON_UP = 2;
-const int BUTTON_DOWN = 3;
-const int BUTTON_DEBOUNCE = 120;
+#ifdef ADAFRUIT_TRINKET_M0
+// setup Dotstar LED on Trinket M0
+#include <Adafruit_DotStar.h> // https://github.com/adafruit/Adafruit_DotStar
+#define DATAPIN 7
+#define CLOCKPIN 8
+Adafruit_DotStar strip = Adafruit_DotStar(1, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+#endif
 
-const int LCD_DIO = 4;
-const int LCD_CLK = 5;
+USBHost usb;
+USBH_MIDI Midi(&usb);
 
-const int CHANNEL_DIP = 4;
-const int DIP_DEBOUNCE = 5000; // 5s in between reading dip
-const int DIP[CHANNEL_DIP] = { 6, 7, 8, 9 };
-const bool dipValues[CHANNEL_DIP] = { 0, 0, 0, 0 };
+const int BUTTON_UP = 0;
+const int BUTTON_DOWN = 1;
+const int BUTTON_DEBOUNCE = 140; // Any shorter and we get double-presses. Any longer it's sluggish feeling...
+const int CHANNEL_DEBOUNCE = 1200; // Show "ch00" on the screen for 1200ms
 
-volatile int channel = 0; // maybe this is 1?
-volatile int commandCode = 104; // 103 sends disabled
+volatile int channel = 0; // MIDI channel is 0/1 by default
+// volatile int commandCode = 103; // Bypass
+volatile int commandCode = 104; // Engaged
 
-volatile int preset = 0;
+volatile int preset = 0; // Initial preset value
 
 volatile long buttonTime = 0;
-volatile long dipTime = 0;
 volatile long displayTime = 0;
 
 // instantiate display
+const int LCD_DIO = 2;
+const int LCD_CLK = 3;
 TM1637Display display(LCD_CLK, LCD_DIO);
 
 void changePreset(int value) {
-  preset = value;
-  
-  USBMIDI.write(0xB0 | (channel & 0xf));
-  USBMIDI.write(commandCode & 0x7f);
-  USBMIDI.write(value & 0x7f);
+  preset = value; // Set globally for display
+  uint8_t buf[3] = {
+    0xB0 | (channel & 0xf),
+    commandCode & 0x7f,
+    value & 0x7f
+  };
+  Midi.SendData(buf);
 }
 
-// from https://stackoverflow.com/a/32410344
-int getChannel() {
-  int newChannel = 0;
-  int tmp;
+int getNewValue(bool up, int max, int value) {
+  volatile int newValue = value;
 
-  for (int i = 0; i < CHANNEL_DIP; i++) {
-    tmp = getValue(DIP[i]);
-    newChannel |= tmp << (CHANNEL_DIP - i - 1);
+  if (up) {
+    newValue += 1;
+
+    if (newValue > max) {
+      return 0;
+    }
+
+    return newValue;
   }
 
-  return newChannel;
+  // else down
+  newValue -= 1;
+
+  if (newValue < 0) {
+    return max;
+  }
+
+  return newValue;
 }
 
 int getNewPreset(bool up) {
   int max = 127;
-  int newPreset = preset;
+  return getNewValue(up, max, preset);
+}
 
-  if (up) {
-    newPreset += 1;
-
-    if (newPreset > max) {
-      return 0;
-    }
-
-    return newPreset;
-  }
-
-  // else down
-  newPreset -= 1;
-
-  if (newPreset < 0) {
-    return max;
-  }
-
-  return newPreset;
+int getNewChannel() {
+  int max = 15;
+  return getNewValue(true, max, channel);
 }
 
 bool getValue(int pin) {
   return digitalRead(pin) == LOW;
+}
+
+void showChannel() {
+  volatile int tens = 0;
+  volatile int ones = channel;
+
+  if (channel >= 10) {
+    tens = 1;
+    ones = channel - 10;
+  }
+  
+  uint8_t channelSeg[] = {
+    SEG_D | SEG_E | SEG_G, // "c"
+    SEG_C | SEG_E | SEG_F | SEG_G, // "h"
+    display.encodeDigit(tens),
+    display.encodeDigit(ones)
+  };
+
+  display.setSegments(channelSeg);
+  
+  delay(CHANNEL_DEBOUNCE);
 }
 
 void readButtons() {
@@ -85,28 +110,23 @@ void readButtons() {
   bool upPressed = getValue(BUTTON_UP);
   bool downPressed = getValue(BUTTON_DOWN);
 
-  if (upPressed) {
-    newPreset = getNewPreset(true);
-  }
-  if (downPressed) {
-    newPreset = getNewPreset(false);
-  }
-
-  if ((upPressed || downPressed) && prevPreset != newPreset) {
-    changePreset(newPreset);
+  if (upPressed && downPressed) {
+    // channel = getNewChannel(); // Uncomment this if we ever desire to change channel?
+    showChannel();
+  } else {
+    if (upPressed) {
+      newPreset = getNewPreset(true);
+    }
+    if (downPressed) {
+      newPreset = getNewPreset(false);
+    }
+  
+    if ((upPressed || downPressed) && prevPreset != newPreset) {
+      changePreset(newPreset);
+    }
   }
 
   buttonTime = millis();
-}
-
-void readDip() {
-  if ((millis() - dipTime) < DIP_DEBOUNCE) {
-    return;
-  }
-
-  channel = getChannel();
-
-  dipTime = millis();
 }
 
 void updateDisplay() {
@@ -114,36 +134,38 @@ void updateDisplay() {
     return;
   }
 
-  display.showNumberDec(preset, false);
+  display.showNumberDec(preset);
 
   displayTime = millis();
 }
 
 void setup() {
+  // Turn off built-in RED LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+#ifdef ADAFRUIT_TRINKET_M0
+  // Turn off built-in Dotstar RGB LED
+  strip.begin();
+  strip.clear();
+  strip.show();
+#endif
+
+  usb.Init();
+
   pinMode(BUTTON_UP, INPUT_PULLUP);
   pinMode(BUTTON_DOWN, INPUT_PULLUP);
 
-  for (int i = 0; i < CHANNEL_DIP; i++) {
-    pinMode(DIP[i], INPUT_PULLUP);
-  }
-  
-  channel = getChannel();
-
   display.clear();
-  display.setBrightness(4);
+  display.setBrightness(3);
+
+  showChannel();
+
+  delay(200);
 }
 
 void loop() {
-  USBMIDI.poll();
-
-  while (USBMIDI.available()) {
-    USBMIDI.read();
-  }
+  usb.Task();
 
   readButtons();
-  readDip();
   updateDisplay();
-
-  USBMIDI.flush();
 }
-
